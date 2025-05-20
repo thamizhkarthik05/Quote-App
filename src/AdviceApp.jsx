6ae8d888e5ea2e5311a42293842481e137c08ed9
thamizhkarthik05 <kaarthik.m2005@gmail.com>
2025-05-20 12:36:59 +0530

    First Commit

---
+++ src/AdviceApp.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+
+
+export const AdviceApp = () => {
+
+    const [advice,setAdvice] = useState("Hit the button to get Motivation!");
+    const [count,setCount] = useState(0);
+
+    async function getAdvice()
+    {
+        const response = await fetch("https://api.adviceslip.com/advice");
+        const data = await response.json();
+        setAdvice(data.slip.advice);
+        setCount((c) => c+1);
+        
+    }
+  return (
+
+    <>
+      <div>
+          <h3>{advice}</h3>
+          <button onClick={getAdvice} >Generate Quote</button>
+          <p>You have read <b>{count}</b> qoutes!</p>
+      </div>
+
+      <div className="foot">
+            <h2>Developed By M Kaarthik</h2>
+      </div>
+
+    </>
+  )
+}
+ 
